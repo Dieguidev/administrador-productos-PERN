574ffa7801b13285900341b8cfd401f45ef39328
Dieguidev <diegogaraycullas@gmail.com>
2024-07-26 16:59:41 -0500

    feat: Add validation to createProduct endpoint

--- server/src/handlers/product.ts
+++ server/src/handlers/product.ts
@@ -1,18 +1,10 @@
 import { Request, Response } from "express";
 import Product from "../models/Product.model";
-import { check, validationResult } from "express-validator";
+import {  validationResult } from "express-validator";
 
 
 export const createProduct = async (req: Request, res: Response) => {
   const { name, price, availability } = req.body;
-
-  await check('name').notEmpty().withMessage('El nombre del producto no puede ir vacío').run(req);
-  await check('price')
-    .notEmpty().withMessage('El precio del producto no puede ir vacío')
-    .isNumeric().withMessage('Valor no válidoo')
-    .custom(value => value > 0).withMessage('Precio no válido')
-    .run(req);
-
   let errors = validationResult(req);
 
   if (!errors.isEmpty()) {
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { createProduct } from "./handlers/product";
 
 
@@ -8,4 +9,10 @@ router.get("/", (req, res) => {
   res.json("Hello, World!");
 });
 
-router.post("/products", createProduct);
+router.post("/products",
+  body('name').notEmpty().withMessage('El nombre del producto no puede ir vacío'),
+  body('price')
+    .notEmpty().withMessage('El precio del producto no puede ir vacío')
+    .isNumeric().withMessage('Valor no válidoo')
+    .custom(value => value > 0).withMessage('Precio no válido'),
+  createProduct);
