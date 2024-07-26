e13b9a03d1583e24f71f021d3f924eaaf4979bb8
Dieguidev <diegogaraycullas@gmail.com>
2024-07-26 15:25:53 -0500

    feat: Add validation to createProduct endpoint

--- server/src/handlers/product.ts
+++ server/src/handlers/product.ts
@@ -1,10 +1,35 @@
 import { Request, Response } from "express";
 import Product from "../models/Product.model";
+import { check, validationResult } from "express-validator";
 
 
 export const createProduct = async (req: Request, res: Response) => {
-  const {name, price, availability} = req.body;
+  const { name, price, availability } = req.body;
+
+  await check('name').notEmpty().withMessage('El nombre del producto no puede ir vacío').run(req);
+  await check('price')
+    .notEmpty().withMessage('El precio del producto no puede ir vacío')
+    .isNumeric().withMessage('Valor no válidoo')
+    .custom(value => value > 0).withMessage('Precio no válido')
+    .run(req);
+
+  let errors = validationResult(req);
+
+
+
+
+
+
+
+
+
+  .
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   const product = await Product.create(req.body);
 
-  res.json({data: product});
+  res.json({ data: product });
 }
