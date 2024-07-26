6a130c87013ef70fc6f07c997c7971fbeb805860
Dieguidev <diegogaraycullas@gmail.com>
2024-07-26 15:26:36 -0500

    feat: Remove unnecessary code in createProduct endpoint

--- server/src/handlers/product.ts
+++ server/src/handlers/product.ts
@@ -15,16 +15,6 @@ export const createProduct = async (req: Request, res: Response) => {
 
   let errors = validationResult(req);
 
-
-
-
-
-
-
-
-
-  .
-
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
