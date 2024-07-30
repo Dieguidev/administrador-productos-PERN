32697d916481ed3a7a1a9567858f1e8d140dcf08
Dieguidev <diegogaraycullas@gmail.com>
2024-07-30 09:41:01 -0500

    feat: Update layout component and router configuration

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -1,11 +1,18 @@
-import { Outlet } from "react-router-dom"
-
+import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
   return (
     <>
-    <div>Desde layout</div>
-    <Outlet/>
+      <header className="bg-slate-800">
+        <div className="mx-auto max-w-6xl py-10">
+          <h1 className="text-white text-4xl font-extrabold">
+            Administrador de Productos
+          </h1>
+        </div>
+      </header>
+      <main className="mt-10 mx-auto max-w-6xl p-10 bg-white shadow">
+        <Outlet />
+      </main>
     </>
-  )
-}
+  );
+};
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,17 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./layouts/Layout";
 import { Products } from "./views/Products";
-
+import { NewProduct } from "./views/NewProduct";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout/>,
+    element: <Layout />,
     children: [
       {
         index: true,
-        element: <Products/>
-      }
-    ]
-  }
-])
+        element: <Products />,
+      },
+      {
+        path: "products/nuevo",
+        element: <NewProduct />,
+      },
+    ],
+  },
+]);
