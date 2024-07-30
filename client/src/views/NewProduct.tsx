9d77c9a482e6cdad90f9e1bf84e5e8bc9b66c7b8
Dieguidev <diegogaraycullas@gmail.com>
2024-07-30 10:17:07 -0500

    feat: Update page titles and add navigation links

--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,7 +1,17 @@
-
+import { Link } from "react-router-dom";
 
 export const NewProduct = () => {
   return (
-    <div>NewProduct</div>
-  )
-}
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500">Registrar Producto</h2>
+        <Link
+          to={"/"}
+          className="rounded-md bg-indigo-600 text-white p-3 text-sm font-bold shadow-sm hover:bg-indigo-500"
+        >
+          Volver a productos
+        </Link>
+      </div>
+    </>
+  );
+};
--- client/src/views/Products.tsx
+++ client/src/views/Products.tsx
@@ -1,7 +1,17 @@
-
+import { Link } from "react-router-dom";
 
 export const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500">Productos</h2>
+        <Link
+          to={"/products/nuevo"}
+          className="rounded-md bg-indigo-600 text-white p-3 text-sm font-bold shadow-sm hover:bg-indigo-500"
+        >
+          Agregar producto
+        </Link>
+      </div>
+    </>
+  );
+};
