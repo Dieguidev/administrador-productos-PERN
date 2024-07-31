5e424a79833d2c1469371531ced0d084a17ffcbf
Dieguidev <diegogaraycullas@gmail.com>
2024-07-30 22:32:35 -0500

    feat: Update ProductDetails component to include edit button and navigate to edit page

--- client/src/views/EditProducts.tsx
+++ client/src/views/EditProducts.tsx
@@ -1,33 +1,41 @@
-import { ActionFunctionArgs, Form, Link, redirect, useActionData } from "react-router-dom";
+import {
+  ActionFunctionArgs,
+  Form,
+  Link,
+  redirect,
+  useActionData,
+  useLocation,
+} from "react-router-dom";
 import { ErrorMessage } from "../components/ErrorMessage";
 import { addProduct } from "../services/ProductService";
 
-export async function newProductAction({request}: ActionFunctionArgs){
-  const data = Object.fromEntries(await request.formData())
-  let error = '';
-  if(Object.values(data).includes('')) {
-    error = 'Todos los campos son obligatorios';
+export async function newProductAction({ request }: ActionFunctionArgs) {
+  const data = Object.fromEntries(await request.formData());
+  let error = "";
+  if (Object.values(data).includes("")) {
+    error = "Todos los campos son obligatorios";
   }
 
-  if(error.length) {
+  if (error.length) {
     return error;
   }
 
   await addProduct(data);
 
-  return redirect('/');
+  return redirect("/");
 }
 
 export const EditProduct = () => {
-
   //*trae el error de la funcion newProductAction
-  const error = useActionData() as string
+  const error = useActionData() as string;
+
+  const { state } = useLocation();
 
   return (
     <>
       <div className="flex justify-between">
         <h2 className="text-4xl font-black text-slate-500">
-          Registrar Producto
+          Editar Producto
         </h2>
         <Link
           to={"/"}
@@ -39,8 +47,7 @@ export const EditProduct = () => {
 
       {error && <ErrorMessage>{error}</ErrorMessage>}
 
-      <Form className="mt-10"
-      method="POST">
+      <Form className="mt-10" method="POST">
         <div className="mb-4">
           <label className="text-gray-800" htmlFor="name">
             Nombre Producto:
@@ -51,6 +58,7 @@ export const EditProduct = () => {
             className="mt-2 block w-full p-3 bg-gray-50"
             placeholder="Nombre del Producto"
             name="name"
+            defaultValue={state.product.name}
           />
         </div>
         <div className="mb-4">
@@ -63,6 +71,7 @@ export const EditProduct = () => {
             className="mt-2 block w-full p-3 bg-gray-50"
             placeholder="Precio Producto. ej. 200, 300"
             name="price"
+            defaultValue={state.product.price}
           />
         </div>
         <input
