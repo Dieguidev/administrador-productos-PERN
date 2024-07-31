e7d6e743017d7e2383aa572b5d61aa7a8a3a5a5f
Dieguidev <diegogaraycullas@gmail.com>
2024-07-31 10:48:37 -0500

    Update server listen to use dynamic port number

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import colors from 'colors';
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log(colors.cyan.bold (`Server is running on http://localhost:${PORT}`));
 
 });
